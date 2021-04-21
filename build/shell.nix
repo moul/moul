49995cb6acdaed1ffc6e571b2d521ef151a7381c
Manfred Touron <94029+moul@users.noreply.github.com>
2021-04-21 16:44:33 +0000

    chore: new tryout to install nokogiri

--- build/shell.nix
+++ build/shell.nix
@@ -1,8 +1,9 @@
 # can't run with --pure because we requires system's ssl certs
 let
   pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/470e4a9bbc98b171a7e733dfc9e62531f7b9ceca.tar.gz") {};
-  ruby = pkgs.ruby_3_0;
+  ruby = pkgs.ruby_2_7;
   bundler = pkgs.bundler.override { inherit ruby; };
+  nokogiri = pkgs.rubyPackages.nokogiri.override { inherit ruby; };
 in
 pkgs.mkShell {
   buildInputs = with pkgs; [
@@ -12,13 +13,24 @@ pkgs.mkShell {
     go
     ruby
     bundler
+
+    # nokogiri
+    libxml2
+    libiconv
+    libxslt
+    zlib
+    nokogiri
   ];
   shellHook = ''
-    set -e
+    set -xe
     mkdir -p .local-data/gems
     export GEM_HOME=$PWD/.local-data/gems
     export GEM_PATH=$GEM_HOME
-    #curl -Lks 'https://git.io/rg-ssl' | ruby
+    export PATH=$GEM_HOME/bin:$PATH
+    export XSLT_LIB=${pkgs.lib.makeLibraryPath [ pkgs.libxslt ]}
+    export XML2_LIB=${pkgs.lib.makeLibraryPath [ pkgs.libxml2 ]}
+    # curl -Lks 'https://git.io/rg-ssl' | ruby
+    #bundle config build.nokogiri --use-system-libraries --with-xslt-lib=$XSLT_LIB --with-xml2-lib=$XML2_LIB
     gem install githubchart
   '';
 }
